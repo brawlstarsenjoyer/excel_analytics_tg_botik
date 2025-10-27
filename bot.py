e0f92f703d8ce85c8ea9ffe4e9972ff849d85782
brawlstarsenjoyer <ugukovicugu0@gmail.com>
2025-10-27 19:29:59 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -249,7 +249,7 @@ def main():
 
         # Регистрация обработчиков
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.Document, handle_document))
+    app.add_handler(MessageHandler(filters.Document.ALL, handle_document))
     
     # Глобальный обработчик ошибок
     app.add_error_handler(error_handler)
